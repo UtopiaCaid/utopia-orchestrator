a608cf8c79b676d455ae7255aa9f4bbcccee10f2
Chris Quintanilla <christopher.quintanilla@smoothstack.com>
2021-04-08 13:14:10 -0400

    JTN-SUC-10 feat: Created fron end endpoints for existing authentication and account microservice calls.

--- src/main/java/com/caid/utopia/UtopiaApplication.java
+++ src/main/java/com/caid/utopia/UtopiaApplication.java
@@ -2,6 +2,9 @@ package com.caid.utopia;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class UtopiaApplication {
@@ -10,4 +13,9 @@ public class UtopiaApplication {
 		SpringApplication.run(UtopiaApplication.class, args);
 	}
 
+	@Bean
+	@LoadBalanced
+	public RestTemplate getRT() {
+		return new RestTemplate();
+	}
 }
