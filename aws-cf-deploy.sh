14ab09592651ea55e0ae1477058476aed172daae
Chris Quintanilla <christopher.quintanilla@smoothstack.com>
2021-04-15 18:03:03 -0400

    feat: Implemented Cloud Formation for Orchestrator micro-service.

---
+++ aws-cf-deploy.sh
@@ -0,0 +1,7 @@
+aws cloudformation deploy --region us-east-2 --stack-name UtopiaEurekaMS \
+--template-file utopia-cftemplate --parameter-overrides ApplicationName=UtopiaEurekaMS \
+ECRepositoryUri=$AWS_ID/utopia-eureka:$COMMIT_HASH DBUrl=$DB_URL DBUsername=$DB_USERNAME \
+DBPassword=$DB_PASSWORD ECSCluster=$UTOPIA_CLUSTER ExecutionRoleArn=$EXECUTION_ROLE_ARN \
+SubnetID=$UTOPIA_PRIVATE_SUBNET_1 TargetGroupArnDev=$TARGETGROUP_UTOPIA_ORCHESTRATOR_DEV_ARN \
+TargetGroupArnProd=$TARGETGROUP_UTOPIA_ORCHESTRATOR_PROD_ARN VpcId=$UTOPIA_PUBLIC_VPC_ID \
+--capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM"
